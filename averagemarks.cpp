66fa7522e0c3ac2e9892955101fbcb0d2210b59a
Adityajyoti002 <adityajyoti2002@gmail.com>
2022-08-01 12:24:32 +0530

    msg msg msg

---
+++ averagemarks.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int physics = 80;
+    int chem = 65;
+    int math = 77;
+
+    float avg = (physics + chem + math)/3;
+
+    cout <<"average marks is "<< avg;
+
+
+}
