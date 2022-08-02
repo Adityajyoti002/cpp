b6ecf597dfcef90364a4c4c773362241b54ca5b2
Adityajyoti002 <adityajyoti2002@gmail.com>
2022-08-02 11:24:19 +0530

    new day new code

---
+++ bool.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool x = true;
+    bool isweatherrainy = false;
+
+    cout << x <<endl;
+    cout << isweatherrainy <<endl;
+
+    int y = 3;
+    float pi = 3.1456378;
+    double pi_d = 3.1456378;
+
+    cout<<y<<endl;
+    cout<<pi<<endl;
+    cout<<pi_d<<endl;
+
+    return 0;
+}
